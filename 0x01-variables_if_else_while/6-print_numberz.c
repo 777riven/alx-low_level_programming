75abebf409b7692d3c8ac984da1d2af45da0ab6d
basma <777basima@gmail.com>
2023-03-17 17:29:39 -0700

    ex 6 7 8 9

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - this functon prints all single digit numbers of base 10 starting from 0.
+ * main - this functon prints all single digit numbers of base 10 starting from
+ * 0.
  * Return: 0 in success
 **/
 
