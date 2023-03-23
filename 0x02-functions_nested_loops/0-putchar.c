50cfae58f9b32909c77e72359fbb51ddcf5fc226
basma <777basima@gmail.com>
2023-03-22 18:11:16 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "stdio.h"
+#include "main.h"
+/**
+ * main - prints _putchar + a new line.
+ * Return: 0 in success
+**/
+
+int main(void)
+{
+	int j;
+	char c[] = "_putchar";
+	int a = sizeof(c);
+	for (j = 0; j <= a; j++)
+	{
+		_putchar(c[j]);
+	}
+
+	_putchar('\n');
+
+	return (0);
+}
