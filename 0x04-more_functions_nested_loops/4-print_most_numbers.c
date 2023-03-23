529434953d41ff26c8231b778f1db7c067b9bd16
basma <777basima@gmail.com>
2023-03-23 10:07:00 -0700

    1-2-3-4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,6 +5,7 @@
  *
  * Return: 0-9, excluding 2 and 4, followed by new line
  */
+
 void print_most_numbers(void)
 {
 	int i;
@@ -12,6 +13,7 @@ void print_most_numbers(void)
 	for (i = 0; i <= 9; i++)
 	{
 		if ((i =! 2) && (i =! 4))
+
 			_putchar(i + '0');
 	}
 	_putchar('\n');
