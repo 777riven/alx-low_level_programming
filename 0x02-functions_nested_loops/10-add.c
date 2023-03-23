945e223c5a68da2625c63bd8943bc8038f9dbe3e
basma <777basima@gmail.com>
2023-03-22 19:31:56 -0700

    10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * add - function that adds two integers and returns the result.
+ * 10-add - function that adds two integers and returns the result.
  * Return: result of sum of two numbers
 **/
 
