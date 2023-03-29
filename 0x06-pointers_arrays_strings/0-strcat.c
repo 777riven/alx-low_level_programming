dfb2038f168b9a6b3f0f9985fdf226ed54493fb7
basma <777basima@gmail.com>
2023-03-29 14:49:20 -0700

    update ex 0 and 1

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,7 +10,13 @@
 
 char *_strcat(char *dest, char *src)
 {
-	strcat(dest, src);
+	int dest_len = strlen(dest);
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+		dest[dest_len + i] = src[i];
+
+	dest[dest_len + i] = '\0';
 
 	return (dest);
 }
